2d9f3fdfe9c2b77172e25f4a48a2ef42e77bbb19
Kirill Stepankov <fpm.stepanko@bsu.by>
2021-11-04 15:10:07 +0300

    Change one big function in two

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <vector>
+#include <utility>
 
 using namespace std;
 
@@ -17,11 +19,14 @@ bool IsEmptyLine(const string& str){
     return false;
 }
 
-void FindEmptyLinesInFile(ifstream& inpfile, ofstream& outfile){
-    if (!inpfile || !outfile) {
+
+ vector<pair<int, int>> FindEmptyLinesInFile(ifstream& inpfile){
+    if (!inpfile) {
         cerr << "Something goes wrong with files";
-        return;
+        exit(1);
     }
+
+    vector<pair<int, int>> empty_lines;
     int line_number = 1, empty_line_number;
     int empty_lines_counter = 0;
     while (inpfile) {
@@ -33,17 +38,30 @@ void FindEmptyLinesInFile(ifstream& inpfile, ofstream& outfile){
                 empty_line_number = line_number;
         } else {
             if (empty_lines_counter != 0) {
-                outfile << "Line " << empty_line_number << "; Amount " << empty_lines_counter << endl;
+                empty_lines.push_back({empty_line_number, empty_lines_counter});
                 empty_lines_counter = 0;
             }
         }
         line_number++;
+    }
+    return empty_lines;
+}
+
+void WriteInFile(const vector<pair<int, int>>& lines, ofstream& outfile){
+    if (!outfile) {
+        cerr << "Something goes wrong with files";
+        exit(1);
+    }
+
+    for(const pair<int, int>& line : lines){
+        outfile << "Line " << line.first << "; Amount " << line.second << endl;
     }
 }
 
 int main() {
     ifstream inpfile("input.txt");
     ofstream outfile("output.txt");
-    FindEmptyLinesInFile(inpfile, outfile);  
+    vector<pair<int, int>> lines = FindEmptyLinesInFile(inpfile);
+    WriteInFile(lines, outfile);
     return 0;
 }
